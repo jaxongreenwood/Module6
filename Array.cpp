9b3461adf4b12fc2a600d3ff0b27e57767ea6a13
jaxongreenwood <jaxongreenwood@weber.mail.edu>
2017-10-03 11:56:27 -0600

    Initial commit

---
+++ Array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+const int SIZE = 3;
+
+int main() {
+    // define a array of three integers
+    int age[SIZE];
+    float amount;
+    float total;
+
+    // take input
+    for(int i = 0; i < SIZE; i++){
+        cout << "Enter you age: ";
+        cin >> age[i];
+    }
+
+    for(int i = 0; i < SIZE; i++){
+        cout << "\nAge of i is:" << age[i];
+    }
+
+    cout << "\nDone!" << endl;
+    return 0;
+}
